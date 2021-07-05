904267095dbadabe029cd2ab0649cdb0b911de26
BenjaminSpiecker <benjaminspiecker@gmail.com>
2021-07-05 18:09:13 -0300

    changes

--- client/src/components/login/login.tsx
+++ client/src/components/login/login.tsx
@@ -1,12 +1,27 @@
 import React from 'react';
+import logo from '../../logo.svg';
 import style from './login.module.css';
 
-function login() {
+function Login() {
+
+    function handleSubmit(e) {
+        e.preventDefault()
+        console.log(e)
+    }
+
     return (
-        <div className={style.container}>
-            Componente Login
+        <div className="container">
+            <div>
+                <img src={logo} alt='logo'></img>
+            </div>
+            <div>
+                <h1>INICIAR SESIÓN</h1>
+                <form onSubmit={handleSubmit}>
+                    <input type="submit" value="Enviar" />
+                </form>
+            </div>
         </div>
     )
 }
 
-export default login
+export default Login;
