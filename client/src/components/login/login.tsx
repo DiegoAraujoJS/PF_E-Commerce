626d3833285ce4a97e7667630c5cefc462f8a449
BenjaminSpiecker <benjaminspiecker@gmail.com>
2021-07-05 17:13:51 -0300

    create/component-login
    Creo el componente en el que se va a visualizar el Login

---
+++ client/src/components/login/login.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from './login.module.css';
+
+function login() {
+    return (
+        <div className={style.container}>
+            Componente Login
+        </div>
+    )
+}
+
+export default login
