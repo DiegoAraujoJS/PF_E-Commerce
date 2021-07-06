d1c6de56025dfe9ea26e1b687f54a1296094c022
DiegoAraujoJS <diegolaraujo96@gmail.com>
2021-07-06 12:25:52 -0300

    config de rutas

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,10 +2,16 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Login from './components/login/login';
-
+import {BrowserRouter} from 'react-router-dom'
+import {Route} from 'react-router-dom'
 function App() {
   return (
-    <Login />
+    <BrowserRouter>
+        
+        <Route exact path='/clases' ><div><span>funca</span></div></Route>
+        <Route exact path='/'> <Login /> </Route>
+    </BrowserRouter>
+    // <Login />
   );
 }
 
