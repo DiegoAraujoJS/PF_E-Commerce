ceb63d83190bb0c8656f410baf421783adf02758
BenjaminSpiecker <benjaminspiecker@gmail.com>
2021-07-06 12:16:49 -0300

    Cambio de nombres de los contenedores de clases

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Login from './components/login/login';
+import ClassContainer from './components/classContainer/ClassContainer';
 
 function App() {
   return (
-    <Login />
+    // <Login />
+    <ClassContainer />
   );
 }
 
