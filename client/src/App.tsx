1221c64ff67bc4f30fd262da7b56d3c6f0ca7485
Aanttrax <Arielrt20@gmail.com>
2021-07-06 19:44:42 -0400

    Ariel Agregue componentes de reclamo

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Route } from 'react-router';
 import './App.css';
-import Login from './components/login/login';
+//import Login from './components/login/login';
+import Claims from './components/Claims/Claims';
+import DetailClaim from './components/Claims/DetailClaim';
 
 function App() {
   return (
-    <Login />
+    // <Login />
+    <div>
+      <Route exact path = '/claim'><Claims/></Route>
+      <Route exact path = '/claim/:id'><DetailClaim/></Route>
+    </div>
   );
 }
 
---
+++ client/src/components/Claims/Claim.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import style from './Claim.module.css';
+
+function Claim(props) {
+
+    let { name, description, code } = props;
+
+    return (
+        <div className = {style.container}>
+            <h5>{name}</h5>
+            <p>{description}</p>
+            <span>#{code}</span>
+            <Link className = {style.btn} to = { `/claim/${code}` }> Visualizar </Link>
+        </div>
+    )   
+}
+
+export default Claim;
