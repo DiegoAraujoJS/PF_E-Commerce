fbf0b898f5a98e207b1b55c36a6581dda57c387a
BenjaminSpiecker <benjaminspiecker@gmail.com>
2021-07-06 12:27:17 -0300

    Cambios

--- client/src/components/classCard/ClassCard.tsx
+++ client/src/components/classCard/ClassCard.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function Class() {
+export default function ClassCard() {
     return (
         <div>
             
--- client/src/components/classCards/ClassCards.tsx
+++ client/src/components/classCards/ClassCards.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function Container() {
+export default function ClassCards() {
     return (
         <div>
             
