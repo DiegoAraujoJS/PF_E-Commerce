241219abe4ebee4b42e45b6a40b8a121c823539b
DiegoAraujoJS <diegolaraujo96@gmail.com>
2021-07-07 01:53:45 -0300

    calendario renderizado en la ruta localhost:3000/calendario

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,11 +5,13 @@ import Login from './components/login/login';
 import {BrowserRouter} from 'react-router-dom'
 import {Route} from 'react-router-dom'
 import ClassContainer from './components/classContainer/ClassContainer';
+import CalendarApp from './components/calendar/Calendar';
 function App() {
   return (
     <BrowserRouter>
         <Route exact path='/clases'><ClassContainer /></Route>
         <Route exact path='/'> <Login /> </Route>
+        <Route exact path='/calendar'> <CalendarApp /> </Route>
     </BrowserRouter>
     
   );
