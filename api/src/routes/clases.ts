90ef273e4f465e6b1313de15974e9382b031a725
EdwardBurgos <edwardpbn@gmail.com>
2021-07-08 20:32:28 -0500

    put correjido

--- api/src/routes/clases.ts
+++ api/src/routes/clases.ts
@@ -113,59 +113,125 @@ router.post('/puntuar', async (req: Request, res: Response) => {
 // Actualizar la puntuacion de una clase
 router.put('/puntuar', async (req: Request, res: Response) => {
     try {
-        const { id, alumno, puntuacion, comentario } = req.body;
+    const { id, alumno, puntuacion, comentario } = req.body;
         const clase = await Clase.findOne({
             where: { id },
             include: [{
                 model: Puntuacion,
-                attributes: ['usuario', 'puntuacion', 'comentario'],
+                attributes: ['puntuacion']
             }]
         })
         if (clase) {
+            const profesor = clase.Profesor_mail;
             if (clase.puntuaciones.length) {
-                const puntuacionAlumnoClase = clase.puntuaciones.filter(elemento => elemento.usuario === alumno);
-                if (puntuacionAlumnoClase.length) {
-                    const puntuacionAlumno = await Puntuacion.findOne({
-                        where: {
-                            [Op.and]: [
-                                { usuario: alumno },
-                                { clase: id }
-                            ]
+                const puntuacionAlumnoClase = await Puntuacion.findOne({
+                    where: {
+                        [Op.and]: {
+                            clase: id,
+                            usuario: alumno
                         }
-                    })
-                    if (puntuacionAlumno) {
-                        await puntuacionAlumno.update({
-                            puntuacion,
-                            comentario
-                        })
                     }
-                }
-            } else {
-                await Puntuacion.create({
-                    usuario: alumno,
-                    clase: id,
-                    puntuacion,
-                    comentario
-                });
+                })
+                if (puntuacionAlumnoClase) {
+                    await puntuacionAlumnoClase.update({
+                        puntuacion,
+                        comentario
+                    })
+                } else {
+                    return res.send(`${alumno} aún no ha comentado esta clase si desea cree una nueva puntuacion`)
+                } 
             }
-
-            const claseActualizada = await Puntuacion.findOne({
-                where: {
-                    [Op.and]: [
-                        { usuario: alumno },
-                        { clase: id }
-                    ]
-                }, attributes: ['puntuacion', 'comentario']
+            const puntuaciones = await Puntuacion.findAll({
+                where: {clase: id},
+                attributes: ['puntuacion']
             })
-
-            res.send(claseActualizada)
-        } else {
-            return res.send(`No se encontró ninguna clase con el id ${id}`)
+            console.log(puntuaciones)
+            let puntuacionClase = parseFloat((puntuaciones.map(elemento => elemento.puntuacion).reduce(function(a, b){ return a + b; })/puntuaciones.length).toFixed(2))
+            let claseActualizada = await Clase.findOne({
+                where: { id },
+                include: [{
+                    model: Puntuacion,
+                    required: true,
+                    attributes: ['usuario', 'puntuacion', 'comentario']
+                }]
+            })
+            if (claseActualizada) {
+                claseActualizada = await claseActualizada.update({ puntuacion: puntuacionClase })
+            }
+            let clasesProfesor = await Clase.findAll({
+                where: {Profesor_mail: profesor},
+                attributes: ['puntuacion']
+            })
+            console.log(clasesProfesor)
+            let puntuacionProfesor = parseFloat((clasesProfesor.map(elemento => elemento.puntuacion).reduce(function(a, b){ return a + b; })/clasesProfesor.length).toFixed(2))
+            console.log(clasesProfesor.map(elemento => elemento.puntuacion))
+            let profesorPorActualizar = await Profesor.findOne({ where: { User_mail: profesor }});
+            if (profesorPorActualizar) {
+                profesorPorActualizar = await profesorPorActualizar.update({ puntuacion: puntuacionProfesor })
+            }
+            return res.send(claseActualizada)
         }
+        return res.send(`No se encontró ninguna clase con el id ${id}`)
     } catch (error) {
-        return res.send(error)
+        return res.send(error);
     }
 })
+
+// router.put('/puntuar', async (req: Request, res: Response) => {
+//     try {
+//         const { id, alumno, puntuacion, comentario } = req.body;
+//         const clase = await Clase.findOne({
+//             where: { id },
+//             include: [{
+//                 model: Puntuacion,
+//                 attributes: ['usuario', 'puntuacion', 'comentario'],
+//             }]
+//         })
+//         if (clase) {
+//             if (clase.puntuaciones.length) {
+//                 const puntuacionAlumnoClase = clase.puntuaciones.filter(elemento => elemento.usuario === alumno);
+//                 if (puntuacionAlumnoClase.length) {
+//                     const puntuacionAlumno = await Puntuacion.findOne({
+//                         where: {
+//                             [Op.and]: [
+//                                 { usuario: alumno },
+//                                 { clase: id }
+//                             ]
+//                         }
+//                     })
+//                     if (puntuacionAlumno) {
+//                         await puntuacionAlumno.update({
+//                             puntuacion,
+//                             comentario
+//                         })
+//                     }
+//                 }
+//             } else {
+//                 await Puntuacion.create({
+//                     usuario: alumno,
+//                     clase: id,
+//                     puntuacion,
+//                     comentario
+//                 });
+//             }
+
+//             const claseActualizada = await Puntuacion.findOne({
+//                 where: {
+//                     [Op.and]: [
+//                         { usuario: alumno },
+//                         { clase: id }
+//                     ]
+//                 }, attributes: ['puntuacion', 'comentario']
+//             })
+
+//             res.send(claseActualizada)
+//         } else {
+//             return res.send(`No se encontró ninguna clase con el id ${id}`)
+//         }
+//     } catch (error) {
+//         return res.send(error)
+//     }
+// })
 
 
 
