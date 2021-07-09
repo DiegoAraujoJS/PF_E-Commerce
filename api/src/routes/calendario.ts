5b67e611a0ff24210d60bdbed7f968d86ec52f72
braian-97 <braian.silva97@gmail.com>
2021-07-09 19:48:19 -0300

    cambiar algo

--- api/src/routes/calendario.ts
+++ api/src/routes/calendario.ts
@@ -265,17 +265,17 @@ router.put('/edit', async (req: Request, res: Response) => {
                             if (query.ocupado) {
                                 let sumaHorario_1 = h[0].substring(0, 2) + h[0].substring(3, 5) + h[0].substring(6, 8)
                                 let sumaHorario_2 = h[1].substring(0, 2) + h[1].substring(3, 5) + h[1].substring(6, 8)
-                                let sumaHorarioDisponible_1 = query.ocupado[0][0].substring(0, 2) + query.ocupado[0][0].substring(3, 5) + query.ocupado[0][0].substring(6, 8)
-                                let sumaHorarioDisponible_2 = query.ocupado[0][1].substring(0, 2) + query.ocupado[0][1].substring(3, 5) + query.ocupado[0][1].substring(6, 8)
+                                let sumaHorarioOcupado_1 = query.ocupado[0][0].substring(0, 2) + query.ocupado[0][0].substring(3, 5) + query.ocupado[0][0].substring(6, 8)
+                                let sumaHorarioOcupado_2 = query.ocupado[0][1].substring(0, 2) + query.ocupado[0][1].substring(3, 5) + query.ocupado[0][1].substring(6, 8)
 
                                 
-                                if(sumaHorario_1 > sumaHorarioDisponible_1 && sumaHorario_2 < sumaHorarioDisponible_2){
+                                if(sumaHorario_1 > sumaHorarioOcupado_1 && sumaHorario_2 < sumaHorarioOcupado_2){
                                     return null
                                 }
-                                else if (sumaHorario_2 > sumaHorarioDisponible_1 && sumaHorario_2 < sumaHorarioDisponible_2) {
+                                else if (sumaHorario_2 > sumaHorarioOcupado_1 && sumaHorario_2 < sumaHorarioOcupado_2) {
                                     return [h[0], query.ocupado[0][0]]
                                 }
-                                else if (sumaHorario_1 < sumaHorarioDisponible_2 && sumaHorario_2 > sumaHorarioDisponible_2) {
+                                else if (sumaHorario_1 < sumaHorarioOcupado_2 && sumaHorario_2 > sumaHorarioOcupado_2) {
                                     return [query.ocupado[0][1], h[1]]
                                 }
                                 else {
