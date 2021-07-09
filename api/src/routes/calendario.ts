30e477ccf3dadb276fc3661af4c917407f92ff50
braian-97 <braian.silva97@gmail.com>
2021-07-09 17:48:49 -0300

    arreglar ruta de calendario

--- api/src/routes/calendario.ts
+++ api/src/routes/calendario.ts
@@ -3,8 +3,8 @@ import Clase from './../models/Clase'
 import Profesor from '../models/Profesor'
 import User from '../models/Usuario';
 import { Op, where } from 'sequelize'
+import { isNullishCoalesce } from 'typescript';
 const router = Router()
-
 
 
 // TodasLasSemanas lo puede mandar el front si el profesor pone ''tengo disponibles los lunes de 14 a 18''
@@ -76,6 +76,51 @@ interface Horario {
 //     }
 // })
 
+//------------------------------ FUNCION --------------------------------
+
+var nuevosHorarios = (arrayHorarios: Array<string[]>) => {
+
+    let horarioNumero = arrayHorarios.map(h => {
+        if (h) {
+            let sumaHorario_1 = h[0].substring(0, 2) + h[0].substring(3, 5) + h[0].substring(6, 8)
+            let sumaHorario_2 = h[1].substring(0, 2) + h[1].substring(3, 5) + h[1].substring(6, 8)
+            return [sumaHorario_1, sumaHorario_2]
+        }
+    })
+
+    let numeros = horarioNumero && horarioNumero.map(elements => elements && elements.join().split(","))
+
+    let ordenados = numeros && numeros.join().split(",").sort()
+
+    let eliminarRepetidos = ordenados?.filter((item, index) => {
+        return ordenados?.indexOf(item) === index;
+    })
+
+    let horariosNuevos = eliminarRepetidos?.map(h => {
+        if (h) {
+            let horario = h.substring(0, 2) + ":" + h.substring(2, 4) + ":" + h.substring(4, 6)
+            return horario
+        }
+    })
+
+    let horariosTuplas: any[] = []
+    horariosNuevos?.forEach((e, i) => {
+        horariosTuplas.push([horariosNuevos && horariosNuevos[i], horariosNuevos && horariosNuevos[i + 1]])
+        i += 2
+    })
+
+    let completarHorario = horariosTuplas.map(e => {
+        if (!e[1]) {
+            return [e[0], e[0]]
+        }
+        else {
+            return e
+        }
+    })
+
+    return completarHorario
+}
+//----------------------------------------- TERMINA LA FUNCION -------------------------------------------
 
 router.post('/add', async (req: Request, res: Response) => {
     let query: Horario = req.body
@@ -87,75 +132,31 @@ router.post('/add', async (req: Request, res: Response) => {
         })
 
         if (profesor) {
+
             if (profesor.calendario) {
                 let indice = profesor.calendario.findIndex(element => element.fecha.anio === query.fecha.anio && element.fecha.mes === query.fecha.mes && element.fecha.dia === query.fecha.dia)
 
                 let calendario = profesor.calendario[indice]
 
                 if (calendario) {
-                    if (calendario.disponible && calendario.ocupado) {
-                        let ocupadoEditado = calendario.ocupado.map(h => {
-                            if (query.ocupado) {
-                                let sumaHorario_2 = h[1].substring(0, 2) + h[1].substring(3, 5) + h[1].substring(6, 8)
-                                let sumaHorarioDisponible_1 = query.ocupado[0][0].substring(0, 2) + query.ocupado[0][0].substring(3, 5) + query.ocupado[0][0].substring(6, 8)
-    
-                                let sumaHorario_1 = h[0].substring(0, 2) + h[0].substring(3, 5) + h[0].substring(6, 8)
-                                let sumaHorarioDisponible_2 = query.ocupado[0][1].substring(0, 2) + query.ocupado[0][1].substring(3, 5) + query.ocupado[0][1].substring(6, 8)
 
-                                //"15:45:00"
-                                if (sumaHorario_2 >= sumaHorarioDisponible_1) {
-                                    return [h[0], query.ocupado[0][0]]
-                                }
-                                if (sumaHorario_1 <= sumaHorarioDisponible_2) {
-                                    return [query.ocupado[0][1], h[1]]
-                                }
-                                else {
-                                    return h
-                                }
-                            }
-                            else {
-                                return h
-                            }
-                        })
-                        let disponibleEditado = calendario.disponible.map(h => {
-                            if (query.disponible) {
-                                let sumaHorario_2 = h[1].substring(0, 2) + h[1].substring(3, 5) + h[1].substring(6, 8)
-                                let sumaHorarioOcupado_1 = query.disponible[0][0].substring(0, 2) + query.disponible[0][0].substring(3, 5) + query.disponible[0][0].substring(6, 8)
-
-                                let sumaHorario_1 = h[0].substring(0, 2) + h[0].substring(3, 5) + h[0].substring(6, 8)
-                                let sumaHorarioOcupado_2 = query.disponible[0][1].substring(0, 2) + query.disponible[0][1].substring(3, 5) + query.disponible[0][1].substring(6, 8)
-
-                                console.log("sumaHorario_1", sumaHorario_1)
-                                if (sumaHorario_2 >= sumaHorarioOcupado_1) {
-                                    return [h[0], query.disponible[0][0]]
-                                }
-                                if (sumaHorario_1 <= sumaHorarioOcupado_2) {
-                                    return [query.disponible[0][1], h[1]]
-                                }
-                                else {
-                                    return h
-                                }
-                            }
-                            else {
-                                return h
-                            }
-                        })
-                        console.log("disponibleEditado", disponibleEditado)
-                        console.log("ocupadoEditado", ocupadoEditado)
-
-                        profesor.set({
-                            ...profesor,
-                            calendario: [
-                                {
-                                    fecha: query.fecha,
-                                    disponible: [...disponibleEditado, query.disponible[0]],
-                                    ocupado: [...ocupadoEditado, query.ocupado ? query.ocupado[0] : null],
-                                }
-                            ]
-                        })
-                        let calendarioEditado = await profesor.save()
-                        res.send(calendarioEditado)
+                    let nuevaFecha = {
+                        email: query.email,
+                        fecha: query.fecha,
+                        disponible: calendario.disponible ? query.disponible ? nuevosHorarios([...calendario.disponible, query.disponible[0]]) : calendario.disponible : null,
+                        ocupado: calendario.ocupado ? query.ocupado ? nuevosHorarios([...calendario.ocupado, query.ocupado[0]]) : calendario.ocupado : null,
                     }
+
+                    let nuevoCalendario = profesor.calendario.map((e, i) => i === indice ? nuevaFecha : e)
+
+                    profesor.set({
+                        ...profesor,
+                        calendario: nuevoCalendario
+                    })
+                    let calendarioEditado = await profesor.save()
+
+                    res.send(calendarioEditado)
+
                 }
                 else {
                     profesor.set({
@@ -163,9 +164,10 @@ router.post('/add', async (req: Request, res: Response) => {
                         calendario: [
                             ...profesor.calendario,
                             {
+                                email: query.email,
                                 fecha: query.fecha,
-                                disponible: query.disponible,
-                                ocupado: query.ocupado,
+                                disponible: query.disponible ? query.disponible : null,
+                                ocupado: query.ocupado ? query.ocupado : null
                             }
                         ]
                     })
@@ -178,9 +180,10 @@ router.post('/add', async (req: Request, res: Response) => {
                     ...profesor,
                     calendario: [
                         {
+                            email: query.email,
                             fecha: query.fecha,
                             disponible: query.disponible ? query.disponible : null,
-                            ocupado: query.ocupado,
+                            ocupado: query.ocupado ? query.ocupado : null,
                         }
                     ]
                 })
@@ -235,34 +238,45 @@ router.put('/edit', async (req: Request, res: Response) => {
                 let calendario = profesor.calendario[indice]
 
                 if (calendario) {
-                    if (calendario.disponible && calendario.ocupado) {
-                        let ocupadoEditado = calendario.ocupado.map(h => {
-                            if (query.disponible) {
-                                //10 - 7
-                                if (h[1].substring(0, 2) > query.disponible[0][0].substring(0, 2)) {
-                                    return [h[0], query.disponible[0][0]]
+                    //------------------------------ FUNCION --------------------------------
+
+                    const editarHorarios = (arrayHorarios: Array<string[]>) => {
+
+                        let completarHorario = arrayHorarios.map(h => {
+                            
+                            if (query.ocupado) {
+                                let sumaHorario_1 = h[0].substring(0, 2) + h[0].substring(3, 5) + h[0].substring(6, 8)
+                                let sumaHorario_2 = h[1].substring(0, 2) + h[1].substring(3, 5) + h[1].substring(6, 8)
+                                let sumaHorarioDisponible_1 = query.ocupado[0][0].substring(0, 2) + query.ocupado[0][0].substring(3, 5) + query.ocupado[0][0].substring(6, 8)
+                                let sumaHorarioDisponible_2 = query.ocupado[0][1].substring(0, 2) + query.ocupado[0][1].substring(3, 5) + query.ocupado[0][1].substring(6, 8)
+
+                                if (sumaHorario_2 > sumaHorarioDisponible_1 && sumaHorario_2 < sumaHorarioDisponible_2) {
+                                    console.log("1", h)
+                                    return [ h[0], query.ocupado[0][0] ]
                                 }
-                                //15 - 17
-                                if (h[0].substring(0, 2) < query.disponible[0][1].substring(0, 2)) {
-                                    return [query.disponible[0][1], h[1]]
+                                else if (sumaHorario_1 < sumaHorarioDisponible_2 && sumaHorario_2 > sumaHorarioDisponible_2) {
+                                    console.log("1", h)
+                                    return [ query.ocupado[0][1] , h[1]]
                                 }
                                 else {
+                                    console.log("2", h)
                                     return h
                                 }
                             }
-                            else {
-                                return h
-                            }
-                        })
-                        let disponibleEditado = calendario.disponible.map(h => {
-                            if (query.ocupado) {
-                                //15 - 16
-                                if (h[1].substring(0, 2) > query.ocupado[0][0].substring(0, 2)) {
-                                    return [h[0], query.ocupado[0][0]]
+                            else if (query.disponible) {
+                                let sumaHorario_1 = h[0].substring(0, 2) + h[0].substring(3, 5) + h[0].substring(6, 8)
+                                let sumaHorario_2 = h[1].substring(0, 2) + h[1].substring(3, 5) + h[1].substring(6, 8)
+                                let sumaHorarioDisponible_1 = query.disponible[0][0].substring(0, 2) + query.disponible[0][0].substring(3, 5) + query.disponible[0][0].substring(6, 8)
+                                let sumaHorarioDisponible_2 = query.disponible[0][1].substring(0, 2) + query.disponible[0][1].substring(3, 5) + query.disponible[0][1].substring(6, 8)
+
+                                if (sumaHorario_2 > sumaHorarioDisponible_1 && sumaHorario_2 < sumaHorarioDisponible_2) {
+                                    console.log("4", h)
+                                    console.log("4 b", [h[0], query.disponible[0][0]])
+                                    return [h[0], query.disponible[0][0]]
                                 }
-                                //5 - 7
-                                if (h[0].substring(0, 2) < query.ocupado[0][1].substring(0, 2)) {
-                                    return [query.ocupado[0][1], h[1]]
+                                else if (sumaHorario_1 < sumaHorarioDisponible_2 && sumaHorario_2 > sumaHorarioDisponible_2) {
+                                    console.log("1", h)
+                                    return [ query.disponible[0][1] , h[1]]
                                 }
                                 else {
                                     return h
@@ -273,10 +287,19 @@ router.put('/edit', async (req: Request, res: Response) => {
                             }
                         })
 
+                        console.log("completarHorario", completarHorario)
+ 
+                        return completarHorario
+                    }
+                    //----------------------------------------- TERMINA LA FUNCION -------------------------------------------
+
+                    if (query.disponible) {
+                        console.log("QUERY DISPONIBLE")
                         let calendarioEditado = {
+                            email: query.email,
                             fecha: query.fecha,
-                            disponible: [...disponibleEditado, query.disponible],
-                            ocupado: [...ocupadoEditado, query.ocupado ? query.ocupado[0] : null],
+                            disponible: calendario.disponible ? nuevosHorarios([...calendario.disponible, query.disponible[0]]) : null,
+                            ocupado: calendario.ocupado ? editarHorarios(calendario.ocupado) : null,
                         }
 
                         let nuevoCalendario = profesor.calendario.map((fecha, i) => i === indice ? calendarioEditado : fecha)
@@ -287,13 +310,30 @@ router.put('/edit', async (req: Request, res: Response) => {
                         })
                         let resultado = await profesor.save()
                         res.send(resultado)
+                    }
+                    else if (query.ocupado) {
+                        console.log("QUERY OCUPADOO")
+                        let calendarioEditado = {
+                            email: query.email,
+                            fecha: query.fecha,
+                            disponible: calendario.disponible ? editarHorarios(calendario.disponible) : null,
+                            ocupado: calendario.ocupado ? nuevosHorarios([...calendario.ocupado, query.ocupado[0]]) : null,
+                        }
 
+                        let nuevoCalendario = profesor.calendario.map((fecha, i) => i === indice ? calendarioEditado : fecha)
+
+                        profesor.set({
+                            ...profesor,
+                            calendario: nuevoCalendario
+                        })
+                        let resultado = await profesor.save()
+                        res.send(resultado)
                     }
                 }
             }
         }
-
     }
+
     catch (error) {
         res.send(error)
     }
