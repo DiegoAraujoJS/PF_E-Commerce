497467260a8c15de941c435c6b56004ce36292d6
braian-97 <braian.silva97@gmail.com>
2021-07-10 16:50:57 -0300

    agregar navbar

--- api/src/routes/session.ts
+++ api/src/routes/session.ts
@@ -11,19 +11,22 @@ router.post('/register', async (req:Request, res:Response) => {
     if (users.find ((user: any) => user.username === username )) {
         return res.status(400).send('el usuario ya existe')
     } else {
-        console.log('register')
+        console.log('register',  req.body)
         users.push(
             {
                 username: username,
                 password: password
             }
         )
+        console.log("users",users)
         return res.send(users[users.length - 1])
     }
 })
-
+console.log("users",users)
 router.post('/login', async (req:Request, res:Response) => {
     const {username, password} = req.body;
+    console.log("req.body",req.body)
+    console.log("users",users)
     if (users.find ((user: any) => (user.username === username && user.password === password))) {
         return res.status(200).send('usuario y contraseña correctos')
     } else {
