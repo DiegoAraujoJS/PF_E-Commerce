166446ed62b61f6a1d389bbf516b1197798b57b3
DiegoAraujoJS <diegolaraujo96@gmail.com>
2021-07-11 12:48:18 -0300

    added interfaces

---
+++ interfaces.ts
@@ -0,0 +1,37 @@
+interface UserProps {
+    name: string;
+    lastName: string;
+    mail: string;
+    role: number;
+}
+
+enum Role {USER, ADMIN}
+
+type ArrayDePares = [`${number}:${number}:00`, `${number}:${number}:00`][]
+
+interface Horario{
+
+    email: string;
+    fecha: {
+        anio: number,
+        mes: number,
+        dia: number
+    },
+    disponible: ArrayDePares,
+    ocupado?: ArrayDePares
+}
+
+type CalendarioResponse = Horario[]
+
+interface ProfesorProps {
+    lastName: string
+    city: string
+    description: string
+    foto: string
+    name: string;
+    User_mail: string;
+    score: number;
+}
+
+
+export {UserProps, Role, ArrayDePares, Horario, CalendarioResponse, ProfesorProps}
