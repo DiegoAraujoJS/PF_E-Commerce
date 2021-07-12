5579b874879ba4b3d5d9d9ebbe548463bba5da73
DiegoAraujoJS <diegolaraujo96@gmail.com>
2021-07-11 22:26:43 -0300

    added refactor on files

---
+++ api/src/routes/nuevosHorarios.ts
@@ -0,0 +1,114 @@
+const nuevosHorarios = (arrayHorarios: Array<string[]>, query?: any) => {
+    console.log("arrayHorarios",arrayHorarios)
+    if (query) {
+        var query_1: string = query[0].substring(0, 2) + query[0].substring(3, 5) + query[0].substring(6, 8)
+        var query_2: string = query[1].substring(0, 2) + query[1].substring(3, 5) + query[1].substring(6, 8)
+    }
+
+    let horarioNumero = arrayHorarios.map(h => {
+        if (h) {
+            if (h[0].length === 8 && h[1].length === 8) {
+                let sumaHorario_1 = h[0].substring(0, 2) + h[0].substring(3, 5) + h[0].substring(6, 8)
+                let sumaHorario_2 = h[1].substring(0, 2) + h[1].substring(3, 5) + h[1].substring(6, 8)
+                return [sumaHorario_1, sumaHorario_2]
+            }
+        }
+    })
+    console.log("horarioNumero",horarioNumero)
+    let numeros = horarioNumero && horarioNumero.map(elements => elements && elements.join().split(","))
+
+    let ordenados = numeros && numeros.join().split(",").sort()
+    console.log("ordenados",ordenados)  
+
+    let eliminarRepetidos = ordenados?.filter((item, index) => {
+        return ordenados?.indexOf(item) === index;
+    })
+
+    let acomodarFechas = eliminarRepetidos?.map((item, index) => {
+        if (query && query_1 && query_2) {
+            if (item > query_1 && item < query_2 ) {
+                if (Number(ordenados[index]) - Number(query_1) < Number(query_2) - Number(ordenados[index])) {
+                    return query_1
+                }
+                else {
+                    return query_2
+                }
+            }
+            else if(item === query_1 && index%2===0){
+                return query_2
+            }
+            else if(item === ordenados[index-1]){
+                return null
+            }
+            else {
+                return item
+            }
+        }
+        else {
+            return item
+        }
+    })
+    console.log("acomodarFechas",acomodarFechas)
+
+    let horariosNuevos = acomodarFechas?.map(h => {
+        if (h) {
+            if (h.length === 6) {
+                let horario = h.substring(0, 2) + ":" + h.substring(2, 4) + ":" + h.substring(4, 6)
+                return horario
+            }
+        }
+    })
+    console.log("horariosNuevos",horariosNuevos)
+
+    let eliminarVacios = horariosNuevos.filter(fecha => fecha)
+    console.log("eliminarVacios",eliminarVacios)
+
+    let horariosTuplas: Array<(string | undefined)[]> = []
+
+    for (let i = 0; i < horariosNuevos.length; i += 2) {
+        horariosTuplas.push([horariosNuevos && horariosNuevos[i], horariosNuevos && horariosNuevos[i + 1]])
+    }
+    console.log("horariosTuplas",horariosTuplas)
+    let completarHorario = horariosTuplas.map(e => {
+        if (!e[1] && e[0]) {
+            if (horariosTuplas && horariosTuplas[horariosTuplas.length - 2][1] && e[0] !== horariosTuplas[horariosTuplas.length - 2][1]) {
+                if (e[0].substring(0, 2) + e[0].substring(3, 5) + e[0].substring(6, 8) > query_2) {
+                    return [query[1], e[0]]
+                }
+                else {
+                    return [horariosTuplas[horariosTuplas.length - 2][1], e[0]]
+                }
+            }
+            else if (horariosTuplas && horariosTuplas[horariosTuplas.length - 2][1] && e[0] === horariosTuplas[horariosTuplas.length - 2][1]) {
+                return [undefined, undefined]
+            }
+            else {
+                return [e[0], e[0]]
+            }
+        }
+        else if (!e[0] && e[1]) {
+            if (horariosTuplas && horariosTuplas[horariosTuplas.length - 2][1] && e[1] !== horariosTuplas[horariosTuplas.length - 2][1]) {
+                if (e[1].substring(0, 2) + e[1].substring(3, 5) + e[1].substring(6, 8) > query_2) {
+                    return [query[1], e[1]]
+                }
+                else {
+                    return [horariosTuplas[horariosTuplas.length - 2][1], e[1]]
+                }
+            }
+            else if (horariosTuplas && horariosTuplas[horariosTuplas.length - 2][1] && e[1] === horariosTuplas[horariosTuplas.length - 2][1]) {
+                return [undefined, undefined]
+            }
+            else {
+                return [e[1], e[1]]
+            }
+        }
+        else {
+            return e
+        }
+    })
+    console.log("completarHorario",completarHorario)
+    let resultado = completarHorario.filter(fecha => fecha[0] && fecha[1])
+
+    return resultado
+}
+export default nuevosHorarios
