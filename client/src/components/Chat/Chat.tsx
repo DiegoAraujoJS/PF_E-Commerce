64a7be1ffface9dbcbbbc1373d18d8f1736237ee
BenjaminSpiecker <benjaminspiecker@gmail.com>
2021-07-14 23:10:23 -0300

    Cambios de estilos en el chat

--- client/src/components/Chat/Chat.tsx
+++ client/src/components/Chat/Chat.tsx
@@ -1,97 +1,78 @@
-import React, { useRef, useState } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import "./Chat.css";
-// import axios from "axios";
-
-import { useAuthState } from "react-firebase-hooks/auth";
+import axios from "axios";
 import { useCollectionData } from "react-firebase-hooks/firestore";
-import { auth, firestore, loginWithGoogle, signOut } from '../../firebase';
-import firebase from 'firebase/app';
+import { firestore } from "../../firebase";
+import firebase from "firebase/app";
+import getCookieValue from "../../cookieParser";
+import { UserProps } from "../../../../interfaces";
+import style from "./Chat.module.css";
 
+function Chat() {
+  const [userLoged, setUserLoged] = useState();
 
-function Chat(props) {
-  const [user] = useAuthState(auth);
-  
+  useEffect(() => {
+    const getUserLoged = async () => {
+      const token = getCookieValue("token").slice(
+        1,
+        getCookieValue("token").length - 1
+      );
+      let userResponse = await axios.post(
+        `http://localhost:3001/api/verify`,
+        {},
+        { withCredentials: true, headers: { Authorization: token } }
+      );
+      setUserLoged(userResponse.data);
+    };
+    getUserLoged();
+  }, []);
 
   return (
-    <div className="App">
-      <header>
-        <h1>⚛️🔥💬</h1>
-        <SignOut />
-      </header>
-
-      <section>{user ? <ChatRoom /> : <SignIn />}</section>
+    <div className={style.appContainer}>
+      {userLoged ? (
+        <ChatRoom userLoged={userLoged} userReference={"admin@admin.com"} />
+      ) : (
+        "no hay usuario logeado"
+      )}
     </div>
   );
 }
 
-function SignIn() {
-  
-  return (
-    <>
-      <button className="sign-in" onClick={loginWithGoogle}>
-        Sign in with Google
-      </button>
-      <p>
-        Do not violate the community guidelines or you will be banned for life!
-      </p>
-    </>
-  );
-}
-
-function SignOut() {
-  return (
-    auth.currentUser && (
-      <button className="sign-out" onClick={signOut}>
-        Sign Out
-      </button>
-    )
-  );
-}
-
-function unificar (mail1, mail2) {
+function unificar(mail1, mail2) {
   let orden;
-  if (mail1<mail2) {orden= mail1+'-'+mail2}
-  else {orden = mail2+'-'+mail1}
-  return orden
+  if (mail1 < mail2) {
+    orden = mail1 + "-" + mail2;
+  } else {
+    orden = mail2 + "-" + mail1;
+  }
+  return orden;
 }
 
-function ChatRoom() {
-  const dummy: any = useRef();
-  // const messagesRef = firestore.collection("messages");
-  // const [user] = useAuthState(auth);
+interface PropsChat {
+  userLoged: UserProps;
+  userReference: String;
+}
 
-  let messagesRef = firestore.collection(unificar("diegolaraujo96@gmail.com", "benjaminspiecker@gmail.com" ));
+function ChatRoom(props: React.PropsWithChildren<PropsChat>) {
+  const dummy: any = useRef();
+
+  let messagesRef = firestore.collection(
+    unificar(props.userLoged.mail, props.userReference)
+  );
 
   const query = messagesRef.orderBy("createdAt").limit(25);
   const [messages] = useCollectionData(query, { idField: "id" });
-  // const [messages, setMessages] = useState([]);
 
   const [formValue, setFormValue] = useState("");
-
-  // useEffect( () => {
-    
-  //   getMessages()
-    
-  // }, [])
-    
-  // const getMessages = async() => {
-  //   const response = await axios.get("http://localhost:3001/api/chats");
-  //    setMessages(response.data);
-  // }
-  // const setMessage = async () => {
-
-  // }
 
   const sendMessage = async (e) => {
     e.preventDefault();
 
-    const { uid, photoURL } = auth.currentUser;
-
     await messagesRef.add({
       text: formValue,
       createdAt: firebase.firestore.FieldValue.serverTimestamp(),
-      uid,
-      photoURL,
+      uid: props.userLoged.mail,
+      photoURL: "http://picsum.photo/200",
     });
 
     setFormValue("");
@@ -100,61 +81,80 @@ function ChatRoom() {
 
   return (
     <>
-      <main>
-        {messages &&
-          messages.map((msg:any, i) => <ChatMessage key={i} message={msg} />)}
+      <div className="App">
+        <nav className="navbar navbar-dark bg-dark">
+          <div className="container text-white fs-5">{props.userReference}</div>
+        </nav>
+        <section
+          className={"d-flex flex-column justify-content-center bg-light"}
+        >
+          <main className={"p-3 d-flex flex-column " + style.main}>
+            {messages &&
+              messages.map((msg: any, i) => (
+                <ChatMessage
+                  key={i}
+                  message={{...msg,  user:props.userLoged.mail}}
+                />
+              ))}
+            <span ref={dummy}></span>
+          </main>
 
-        <span ref={dummy}></span>
-      </main>
+          <form onSubmit={sendMessage} className={"d-flex bg-dark w-100"}>
+            <input
+              value={formValue}
+              onChange={(e) => setFormValue(e.target.value)}
+              placeholder="Envia un mensaje"
+              className={"bg-dark text-light border-0 rounded-0 w-100 px-3 fs-6"}
+            />
 
-      <form onSubmit={sendMessage}>
-        <input
-          value={formValue}
-          onChange={(e) => setFormValue(e.target.value)}
-          placeholder="Envia un mensaje"
-        />
-
-        <button type="submit" disabled={!formValue}>
-          🕊️
-        </button>
-      </form>
-    </>
-  );
-}
-
-function ChatMessage(props) {
-  const { text, uid, photoURL } = props.message;
-  
-  const messageClass = uid === auth.currentUser.uid ? "sent" : "received";
-
-  return (
-    <>
-      <div className={`message ${messageClass}`}>
-        <img
-          alt='alt'
-          src={
-            photoURL || "https://api.adorable.io/avatars/23/abott@adorable.png"
-          }
-        />
-        <p>{text}</p>
+            <button
+              type="submit"
+              disabled={!formValue}
+              className={"px-4 py-3 bg-secondary border-0"}
+            >
+              🕊️
+            </button>
+          </form>
+        </section>
       </div>
     </>
   );
 }
 
-export default Chat;
+function ChatMessage(props) {
+  const { text, uid, photoURL, user } = props.message;
 
-/* Chat: [
-  {
-    id: "uid",
-    name: "email1-email2",
-    data: [
-      {
-        uid: "uid",
-        photoURL: "http://...",
-        text: "hola soy un mensaje",
-        createdAt: Date
-      }
-    ]
-  }
-] */
+  const messageClass = uid === user ? "sent" : "received";
+
+  return (
+    <>
+      {messageClass === "sent" ? (
+        <div className={"d-flex align-items-center flex-row-reverse py-2"}>
+          <img
+            alt="alt"
+            src={
+              photoURL ||
+              "https://api.adorable.io/avatars/23/abott@adorable.png"
+            }
+            className={"rounded-circle p-1 " + style.imgMessage}
+          />
+          <span className={"badge rounded-pill bg-secondary fs-6 text-wrap text-break mw-100"}>{text}</span>
+        </div>
+      ) : (
+        <div className={"d-flex align-items-center flex-row  py-2"}>
+          <img
+            alt="alt"
+            src={
+              photoURL ||
+              "https://api.adorable.io/avatars/23/abott@adorable.png"
+            }
+            className={"rounded-circle p-1 " + style.imgMessage}
+          />
+          <span className={"badge rounded-pill bg-secondary fs-6 text-wrap text-break mw-100"}>{text}</span>
+        </div>
+      )}
+    </>
+  );
+}
+
+export default Chat;
