cd64a08553758a4eea66a95b3163770766edc19d
stringedcat <mauroox09@gmail.com>
2021-07-15 06:29:53 -0300

    filtros y categorias a las clases
    añadido filtros check boxes y categorias a la clses

--- api/src/app.ts
+++ api/src/app.ts
@@ -24,21 +24,21 @@ app.use(session({
     }
 }));
 
-app.use((req, res, next) => {
+/* app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
     next();
-  });
-// app.use(
-//     cors( {
-//         origin: config.cors,
-//         credentials: true,
-//         methods: ['GET', 'POST', 'OPTIONS', 'PUT', 'DELETE'],
-//         allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'authorization']
-//     } )
-// )
+  }); */
+ app.use(
+    cors( {
+       origin: config.cors,
+        credentials: true,
+         methods: ['GET', 'POST', 'OPTIONS', 'PUT', 'DELETE'],
+        allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'authorization']
+    } )
+ )
 
 interface Error {
     message: string;
