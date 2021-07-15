f8948a1346dc600d794373648d78c47477000f4e
BenjaminSpiecker <benjaminspiecker@gmail.com>
2021-07-15 01:05:31 -0300

    cambios

--- client/src/components/Chat/Chat.tsx
+++ client/src/components/Chat/Chat.tsx
@@ -1,12 +1,9 @@
-import React, { useRef, useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import "./Chat.css";
 import axios from "axios";
-import { useCollectionData } from "react-firebase-hooks/firestore";
-import { firestore } from "../../firebase";
-import firebase from "firebase/app";
 import getCookieValue from "../../cookieParser";
-import { UserProps } from "../../../../interfaces";
 import style from "./Chat.module.css";
+import ChatRoom from "./ChatRoom";
 
 function Chat() {
   const [userLoged, setUserLoged] = useState();
@@ -35,125 +32,6 @@ function Chat() {
         "no hay usuario logeado"
       )}
     </div>
-  );
-}
-
-function unificar(mail1, mail2) {
-  let orden;
-  if (mail1 < mail2) {
-    orden = mail1 + "-" + mail2;
-  } else {
-    orden = mail2 + "-" + mail1;
-  }
-  return orden;
-}
-
-interface PropsChat {
-  userLoged: UserProps;
-  userReference: String;
-}
-
-function ChatRoom(props: React.PropsWithChildren<PropsChat>) {
-  const dummy: any = useRef();
-
-  let messagesRef = firestore.collection(
-    unificar(props.userLoged.mail, props.userReference)
-  );
-
-  const query = messagesRef.orderBy("createdAt").limit(25);
-  const [messages] = useCollectionData(query, { idField: "id" });
-
-  const [formValue, setFormValue] = useState("");
-
-  const sendMessage = async (e) => {
-    e.preventDefault();
-
-    await messagesRef.add({
-      text: formValue,
-      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
-      uid: props.userLoged.mail,
-      photoURL: "http://picsum.photo/200",
-    });
-
-    setFormValue("");
-    dummy!.current.scrollIntoView({ behavior: "smooth" });
-  };
-
-  return (
-    <>
-      <div className="App">
-        <nav className="navbar navbar-dark bg-dark">
-          <div className="container text-white fs-5">{props.userReference}</div>
-        </nav>
-        <section
-          className={"d-flex flex-column justify-content-center bg-light"}
-        >
-          <main className={"p-3 d-flex flex-column " + style.main}>
-            {messages &&
-              messages.map((msg: any, i) => (
-                <ChatMessage
-                  key={i}
-                  message={{...msg,  user:props.userLoged.mail}}
-                />
-              ))}
-            <span ref={dummy}></span>
-          </main>
-
-          <form onSubmit={sendMessage} className={"d-flex bg-dark w-100"}>
-            <input
-              value={formValue}
-              onChange={(e) => setFormValue(e.target.value)}
-              placeholder="Envia un mensaje"
-              className={"bg-dark text-light border-0 rounded-0 w-100 px-3 fs-6"}
-            />
-
-            <button
-              type="submit"
-              disabled={!formValue}
-              className={"px-4 py-3 bg-secondary border-0"}
-            >
-              🕊️
-            </button>
-          </form>
-        </section>
-      </div>
-    </>
-  );
-}
-
-function ChatMessage(props) {
-  const { text, uid, photoURL, user } = props.message;
-
-  const messageClass = uid === user ? "sent" : "received";
-
-  return (
-    <>
-      {messageClass === "sent" ? (
-        <div className={"d-flex align-items-center flex-row-reverse py-2"}>
-          <img
-            alt="alt"
-            src={
-              photoURL ||
-              "https://api.adorable.io/avatars/23/abott@adorable.png"
-            }
-            className={"rounded-circle p-1 " + style.imgMessage}
-          />
-          <span className={"badge rounded-pill bg-secondary fs-6 text-wrap text-break mw-100"}>{text}</span>
-        </div>
-      ) : (
-        <div className={"d-flex align-items-center flex-row  py-2"}>
-          <img
-            alt="alt"
-            src={
-              photoURL ||
-              "https://api.adorable.io/avatars/23/abott@adorable.png"
-            }
-            className={"rounded-circle p-1 " + style.imgMessage}
-          />
-          <span className={"badge rounded-pill bg-secondary fs-6 text-wrap text-break mw-100"}>{text}</span>
-        </div>
-      )}
-    </>
   );
 }
 
---
+++ client/src/components/Chat/ChatRoom.tsx
@@ -0,0 +1,94 @@
+import React, { useRef, useState } from "react";
+import "./Chat.css";
+import { useCollectionData } from "react-firebase-hooks/firestore";
+import { firestore } from "../../firebase";
+import firebase from "firebase/app";
+import { UserProps } from "../../../../interfaces";
+import style from "./Chat.module.css";
+import ChatMessage from './ChatMessage';
+
+function unificar(mail1, mail2) {
+  let orden;
+  if (mail1 < mail2) {
+    orden = mail1 + "-" + mail2;
+  } else {
+    orden = mail2 + "-" + mail1;
+  }
+  return orden;
+}
+
+interface PropsChat {
+  userLoged: UserProps;
+  userReference: String;
+}
+
+export default function ChatRoom(props: React.PropsWithChildren<PropsChat>) {
+  const dummy: any = useRef();
+
+  let messagesRef = firestore.collection(
+    unificar(props.userLoged.mail, props.userReference)
+  );
+
+  const query = messagesRef.orderBy("createdAt").limit(25);
+  const [messages] = useCollectionData(query, { idField: "id" });
+
+  const [formValue, setFormValue] = useState("");
+
+  const sendMessage = async (e) => {
+    e.preventDefault();
+
+    await messagesRef.add({
+      text: formValue,
+      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+      uid: props.userLoged.mail,
+      photoURL:
+        "https://tse3.mm.bing.net/th/id/OIP.sFI4WGRjHM9popoUUwLgdwAAAA?pid=ImgDet&w=260&h=280&rs=1",
+    });
+
+    setFormValue("");
+    dummy!.current.scrollIntoView({ behavior: "smooth" });
+  };
+
+  return (
+    <>
+      <div className="App">
+        <nav className="navbar navbar-dark bg-dark">
+          <div className="container text-white fs-5">{props.userReference}</div>
+        </nav>
+        <section
+          className={"d-flex flex-column justify-content-center bg-light"}
+        >
+          <main className={"p-3 d-flex flex-column " + style.main}>
+            {messages &&
+              messages.map((msg: any, i) => (
+                <ChatMessage
+                  key={i}
+                  message={{ ...msg, user: props.userLoged.mail }}
+                />
+              ))}
+            <span ref={dummy}></span>
+          </main>
+
+          <form onSubmit={sendMessage} className={"d-flex bg-dark w-100"}>
+            <input
+              value={formValue}
+              onChange={(e) => setFormValue(e.target.value)}
+              placeholder="Envia un mensaje"
+              className={
+                "bg-dark text-light border-0 rounded-0 w-100 px-3 fs-6"
+              }
+            />
+
+            <button
+              type="submit"
+              disabled={!formValue}
+              className={"px-4 py-3 bg-secondary border-0"}
+            >
+              🕊️
+            </button>
+          </form>
+        </section>
+      </div>
+    </>
+  );
+}
