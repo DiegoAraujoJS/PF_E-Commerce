773b395f2cb4a742baabde3744b010975fd052b3
EdwardBurgos <edwardpbn@gmail.com>
2021-07-18 21:33:23 -0500

    funcionando

--- client/src/components/cesta/Cesta.tsx
+++ client/src/components/cesta/Cesta.tsx
@@ -26,75 +26,43 @@ export default function Cesta() {
             const token = getCookieValue(`token`).replaceAll('\"', '')
             const user = await axios.post(`http://localhost:3001/api/verify`, {}, { headers: { Authorization: token } })
             const clases = await axios.get(`http://localhost:3001/api/carrito/all/${user.data.mail}`)
-
-            // DEVUELVE ESTO
-            //             // [
-            //     {
-            //         "nombre": "Sumas y Restas",
-            //         "descripcion": "Aprende a sumar y restar para ser el mejor de tu clase",
-            //         "esPresencial": "Virtual",
-            //         "grado": "Primer grado",
-            //         "materia": "Matematica",
-            //         "nivel": "Primario",
-            //         "profesor": {
-            //             "score": 2.1,
-            //             "User_mail": "edwardburgos@gmail.com",
-            //             "password": null,
-            //             "name": "Edward",
-            //             "lastName": "Burgos",
-            //             "city": "Lima",
-            //             "foto": "https://images.unsplash.com/photo-1607990281513-2c110a25bd8c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=547&q=80",
-            //             "description": "Profesor apasionado por enseñar",
-            //             "history": null,
-            //             "calendario": null,
-            //             "createdAt": "2021-07-19T01:18:22.817Z",
-            //             "updatedAt": "2021-07-19T01:18:22.817Z"
-            //         },
-            //         "puntuacion": 5,
-            //         "date": {
-            //             "year": 2021,
-            //             "month": 8,
-            //             "day": 7,
-            //             "time": [
-            //                 "08:00:00",
-            //                 "12:00:00"
-            //             ]
-            //         },
-            //         "precio": "$2000"
-            //     }
-            // ]
-            const clasesPorComprarTransform: ClasePorComprar[] = clases.data.map(e => {
-                let clasePorComprar = {
-                    id: 8,
-                    imagen: e.profesor.foto,
-                    nombre: e.profesor.nombre,
-                    precioDescuento: 14.30,
-                    precioOriginal: 16.99,
-                    dia: '16/07/2021',
-                    horaInicio: '9:30 AM',
-                    horaFin: '10:00 PM',
-                    profesor: 'braiansilva@gmail.com'
+            const clasesPorComprarFormateadas: ClasePorComprar[] = clases.data.map(e => {
+                let dia = e.date.day;
+                let mes = e.date.month;
+                let horaInicio = e.date.time[0].split(':');
+                let horaFinal = e.date.time[1].split(':');
+                if (e.date.day.toString().length === 1) dia = '0' + dia;
+                if (e.date.month.toString().length === 1) mes = '0' + mes;
+                if (horaInicio[0] >= 12) {
+                    horaInicio[2] = 'PM';
+                    if (horaInicio[0] !== 12) horaInicio[0] = (Number(horaInicio[0]) - 12).toString();
+                } else {
+                    horaInicio[2] = 'AM';
                 }
+                if (horaFinal[0] >= 12) {
+                    horaFinal[2] = 'PM';
+                    if (horaFinal[0] !== 12) horaFinal[0] = (Number(horaFinal[0]) - 12).toString();
+                } else {
+                    horaFinal[2] = 'AM';
+                }
+                if (horaInicio[0].length === 1) horaInicio[0] = '0' + horaInicio[0];
+                if (horaFinal[0].length === 1) horaFinal[0] = '0' + horaFinal[0];                
+                let clasePorComprar = {
+                    id: e.id, // FALTA
+                    imagen: e.profesor.foto,
+                    nombre: e.nombre,
+                    precioDescuento: Number(e.precio.slice(1)),
+                    precioOriginal: Number(e.precio.slice(1)) + 5000,
+                    dia: `${dia}/${mes}/${e.date.year}`,
+                    horaInicio: `${horaInicio[0]}:${horaInicio[1]} ${horaInicio[2]}`, 
+                    horaFin: `${horaFinal[0]}:${horaFinal[1]} ${horaFinal[2]}`,
+                    profesor: e.profesor.User_mail
+                }
+                return clasePorComprar;
 
             })
-
-            // ME DEVUELVE ESTO > http://localhost:3001/api/carrito/all/edwardburgos@gmail.com
-            // Class ==> ClasePorComprar
-            //   [{
-            //     id: 8, //
-            //     imagen: 'https://images.unsplash.com/photo-1561657819-51c0511e35ab?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=751&q=80',
-            //     nombre: '4 horas de clase de matemáticas',
-            //     precioDescuento: 14.30,
-            //     precioOriginal: 16.99,
-            //     dia: '16/07/2021', 
-            //     horaInicio: '9:30 AM', 
-            //     horaFin: '10:00 PM',
-            //     profesor: 'braiansilva@gmail.com'
-            //   }]
-
-            // `${Number(props.date.time[0].substring(0, 1))}${Number(props.date.time[0].substring(1, 2))}:${Number(props.date.time[0].substring(3, 4))}${Number(props.date.time[0].substring(5, 6))} PM`
             setClient(user.data.mail)
-            setClasesPorComprar(clases.data)
+            setClasesPorComprar(clasesPorComprarFormateadas)
         }
         getClientAndClasses()
     }, [])
